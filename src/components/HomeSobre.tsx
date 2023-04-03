6bef539a2de11cd918bdd94b792197b5e9b4b558
zobek-dev <zobek.dev@gmail.com>
2023-04-03 18:36:53 -0400

    hotfix a por Link

--- src/components/HomeSobre.tsx
+++ src/components/HomeSobre.tsx
@@ -1,4 +1,5 @@
 import { DiamondBig } from "@/svg"
+import Link from "next/link"
 
 export const HomeSobre = () => {
  return(
@@ -6,9 +7,9 @@ export const HomeSobre = () => {
     <div className="wrapper flex flex-col justify-center h-full relative">
       <div className="max-w-[684px]">
         <p className="text-white font-light text-[25px]"><strong className="font-bold">La mejor forma de generar productividad y sustentabilidad,</strong> es involucrando a las personas, manteniendo  procesos optimizados y utilizando los recursos de forma eficiente</p>
-        <a href="/nosotros" className="block mt-4 bg-[#C7E22B] text-[#08376B] text-[17px] font-medium px-4 py-2 w-max rounded-[32px]">
+        <Link href="/nosotros" className="block mt-4 bg-[#C7E22B] text-[#08376B] text-[17px] font-medium px-4 py-2 w-max rounded-[32px]">
           Sobre Optimmet
-        </a>
+        </Link>
       </div>
       <DiamondBig className="absolute -bottom-[28px] lg:left-4"/>
     </div>
