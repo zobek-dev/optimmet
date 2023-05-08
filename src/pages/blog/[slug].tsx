6d36d36c8e8954add81926595c1bef9cce96c432
zobek-dev <zobek.dev@gmail.com>
2023-05-08 10:42:02 -0400

    cambios del share in host

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,6 +1,10 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
+import { GetStaticPaths, GetStaticProps } from "next";
 import axios from "axios";
 import moment from "moment";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { Facebook, Linkedin } from "@/images";
+import { useRouter } from "next/router";
 
 interface Post {
   id: number;
@@ -8,7 +12,7 @@ interface Post {
   content: { rendered: string };
   date: string;
   _embedded: {
-    'wp:featuredmedia'?: {
+    "wp:featuredmedia"?: {
       source_url: string;
     }[];
   };
@@ -24,49 +28,123 @@ interface Props {
 }
 
 
-
 function Post({ post, image, author }: Props) {
-  const published = moment(post.date).format('DD [de] MMMM [de] YYYY'); 
-  console.log(post)
+  const published = moment(post.date).format("DD [de] MMMM [de] YYYY");
+  const [host, setHost] = useState<string | null >(null);
+
+  const router = useRouter();
+  
+  useEffect(()=> {
+    if (typeof globalThis?.window !== undefined){
+      setHost(window.location.host);
+    }
+  },[])
+
+  console.log(host)
+  
   return (
     <div className="bg-white">
       <div className="wrapper">
         <div className="prose lg:prose-xl mx-auto py-28 lg:py-40">
-          <img src={image} alt={post.title.rendered} className="mx-auto rounded-2xl" loading="lazy"/>
-          <h1 className="text-[#556170] font-bold text-[35px] line-height-[1.2] pb-4 lg:pb-8 border-b border-[#62CFF4]" style={{marginBottom:'0'}}>
+          <img
+            src={image}
+            alt={post.title.rendered}
+            className="mx-auto rounded-2xl"
+            loading="lazy"
+          />
+          <h1
+            className="text-[#556170] font-bold text-[35px] line-height-[1.2] pb-4 lg:pb-8 border-b border-[#62CFF4]"
+            style={{ marginBottom: "0" }}
+          >
             {post.title.rendered}
           </h1>
-          <span className="font-light text-[12px] text-center">Publicado {published}</span>
-          <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} className='font-light text-[17px]' />
+          <span className="font-light text-sm text-center block">
+            Publicado {published}
+          </span>
+          <div
+            dangerouslySetInnerHTML={{ __html: post.content.rendered }}
+            className="font-light text-[17px]"
+          />
           <div>
-            <h3 className="text-[#556170] font-semibold leading-none" style={{ fontSize: '20px'}}>Autor</h3>
+            <h3
+              className="text-[#556170] font-semibold leading-none"
+              style={{ fontSize: "20px" }}
+            >
+              Autor
+            </h3>
             <span className="text-[17px] font-light">{author}</span>
           </div>
+          <Link href="/blog/">Volver al Blog</Link>
+          <div className="flex justify-end gap-x-2 items-center">
+            <span className="text-[#556170] text-[20px]">Compartir en:</span>
+            <ul className="flex items-center gap-x-2" style={{ paddingLeft: '0'}}>
+              <Link
+                href={`https://facebook.com/sharer.php?u=${host}${router.asPath}`}
+                rel="noopener noreferrer"
+                className="my-0 transition-opacity duration-200 ease-linear hover:opacity-70"
+                target="_blank"
+              >
+                <img
+                  src={Facebook.src}
+                  width={Facebook.width}
+                  height={Facebook.height}
+                  alt="facebook"
+                  style={{ marginTop: '0', marginBottom: '0'}}
+                  loading="lazy"
+                />
+              </Link>
+              <Link
+                href="https://www.linkedin.com/company/optimmet"
+                rel="noopener noreferrer"
+                className="my-0 transition-opacity duration-200 ease-linear hover:opacity-70"
+                target="_blank"
+              >
+                <img
+                  src={Linkedin.src}
+                  width={Linkedin.width}
+                  height={Linkedin.height}
+                  alt="linkedin"
+                  style={{ marginTop: '0', marginBottom: '0'}}
+                  loading="lazy"
+                />
+              </Link>
+            </ul>
+          </div>
+          
+          <hr className="border-b border-[#62CFF4]"  style={{ marginTop: '0', marginBottom: '0'}}/>
         </div>
       </div>
     </div>
-  );    
+  );
 }
 
-export default Post
+export default Post;
 
 export const getStaticProps: GetStaticProps<Props> = async (context) => {
+  console.log(context)
   const slug = context.params?.slug as string;
-  const response = await axios.get<Post[]>(`${process.env.WPBLOG_URI}wp-json/wp/v2/posts?slug=${slug}`);
+  const response = await axios.get<Post[]>(
+    `${process.env.WPBLOG_URI}wp-json/wp/v2/posts?slug=${slug}`
+  );
   const [post] = response.data;
-  const featuredimage = await axios.get<any>(`${process.env.WPBLOG_URI}wp-json/wp/v2/media/${post.featured_media}`);
-  const dataAuthor = await axios.get<any>(`${process.env.WPBLOG_URI}wp-json/wp/v2/users/${post.author}`)
+  const featuredimage = await axios.get<any>(
+    `${process.env.WPBLOG_URI}wp-json/wp/v2/media/${post.featured_media}`
+  );
+  const dataAuthor = await axios.get<any>(
+    `${process.env.WPBLOG_URI}wp-json/wp/v2/users/${post.author}`
+  );
   const image = featuredimage.data.source_url;
-  const author = dataAuthor.data.name
-  console.log(author)
+  const author = dataAuthor.data.name;
 
   return {
-    props: { post, image, author},
+    props: { post, image, author },
   };
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const response = await axios.get<Post[]>(`${process.env.WPBLOG_URI}wp-json/wp/v2/posts?fields=id,slug`);
+  const response = await axios.get<Post[]>(
+    `${process.env.WPBLOG_URI}wp-json/wp/v2/posts?fields=id,slug`
+  );
   const posts = response.data;
   const paths = posts.map((post) => ({
     params: { slug: post.slug },
